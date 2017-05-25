ffd4ad8be12ee9217e49934ef30642da19c34856
SweetpigMW <com5630019@gmail.com>
2017-05-25 14:55:31 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -21,56 +21,56 @@ $replyToken;
 $temp_set;
 $level_set;
 echo "check 1";
-// // Get POST body content
-// $content = file_get_contents('php://input');
-// // Parse JSON
-// $events = json_decode($content, true);
-// //data_check();
-// echo "check 2";
-// //function data_check(){
-// // Validate parsed JSON data
-// 	if (!is_null($events['events'])) {
-// 		// Loop through each event
-// 		foreach ($events['events'] as $event) {
-// 			// Reply only when message sent is in 'text' format
-// 			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// 				// Get text sent
-// 				$text = $event['message']['text'];
-// 				// Get replyToken
-// 				$replyToken = $event['replyToken'];
-// 				$text_get = explode(",", $text);
-// 				$temp_set = $text_get[0];
-// 				$level_set = $text_get[1];
-// //// check temperature ////
-// if(data_temp <= temp_set){
-// 	echo "check 3";
-// 	$text = "Water temperature is too low";
-// 	// Build message to reply back
-// 	$messages = [
-// 		'type' => 'text',
-// 		'text' => $text
-// 	];
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+//data_check();
+echo "check 2";
+//function data_check(){
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			$text_get = explode(",", $text);
+			$temp_set = $text_get[0];
+			$level_set = $text_get[1];
+			//// check temperature ////
+			if(data_temp <= temp_set){
+				echo "check 3";
+				$text = "Water temperature is too low";
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
 
-// 	// Make a POST Request to Messaging API to reply to sender
-// 	$url = 'https://api.line.me/v2/bot/message/reply';
-// 	$data = [
-// 		'replyToken' => $replyToken,
-// 		'messages' => [$messages],
-// 	];
-// 	$post = json_encode($data);
-// 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-// 	$ch = curl_init($url);
-// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// 	$result = curl_exec($ch);
-// 	curl_close($ch);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
 
-// 	echo $result . "\r\n";
-// }
+				echo $result . "\r\n";
+			}
 // //// check water level ////
 // if(data_level > level_set){
 // 	echo "check 4";
@@ -157,9 +157,9 @@ echo "check 1";
 // // 			curl_close($ch);
 
 // // 			echo $result . "\r\n";
-//  		}
-//  	}
-//  }
+ 		}
+ 	}
+ }
 // echo "OK";
 
 ?>
