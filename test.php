675696c84981ece7aacdaabd86c611beb948ab48
SweetpigMW <com5630019@gmail.com>
2017-05-25 16:43:57 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -53,7 +53,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $temp_set
+					'text' => $temp_set , $level_set
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
@@ -83,7 +83,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $level_set
+					'text' =>  $temp_set , $level_set
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
