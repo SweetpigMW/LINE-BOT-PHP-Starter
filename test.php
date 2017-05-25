e3f8b9f971ccd836ebc178fe3d895b6c90893775
SweetpigMW <com5630019@gmail.com>
2017-05-25 12:51:26 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,59 @@
+<?php
+$access_token = '8lqeJ0Y8ueX9E8IyvAfiRmS7ivqa2sanRuAlakOMG1jXNQVJ6Zem9RsOPe04FoK+T3D45lIAH9v9tJ1fpXh6BqNQL0KLfqJzdASLjDteryhnRhccMGSbOqMIEPaBdAND53w3CfjVr2DLRF9k1sqdDQdB04t89/1O/w1cDnyilFU=';
+
+$data_get = "29.5,2,on";
+$data = explode(",", $data_get);
+$data_temp = $data[0];
+$data_level = $data[1];
+$data_power = $data[2];
+echo $data_temp;
+echo $data_level;
+echo $data_power;
+
+
+// // Get POST body content
+// $content = file_get_contents('php://input');
+// // Parse JSON
+// $events = json_decode($content, true);
+// // Validate parsed JSON data
+// if (!is_null($events['events'])) {
+// 	// Loop through each event
+// 	foreach ($events['events'] as $event) {
+// 		// Reply only when message sent is in 'text' format
+// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// 			// Get text sent
+// 			$text = $event['message']['text'];
+// 			// Get replyToken
+// 			$replyToken = $event['replyToken'];
+
+// 			// Build message to reply back
+// 			$messages = [
+// 				'type' => 'text',
+// 				'text' => $text
+// 			];
+
+// 			// Make a POST Request to Messaging API to reply to sender
+// 			$url = 'https://api.line.me/v2/bot/message/reply';
+// 			$data = [
+// 				'replyToken' => $replyToken,
+// 				'messages' => [$messages],
+// 			];
+// 			$post = json_encode($data);
+// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+// 			$ch = curl_init($url);
+// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 			$result = curl_exec($ch);
+// 			curl_close($ch);
+
+// 			echo $result . "\r\n";
+// 		}
+// 	}
+// }
+// echo "OK";
+
+?>
