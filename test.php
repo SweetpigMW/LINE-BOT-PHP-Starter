d13c51ed718a7c73af965be9c9cf18920fc40b04
SweetpigMW <com5630019@gmail.com>
2017-05-25 15:38:56 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -77,7 +77,7 @@ if (!is_null($events['events'])) {
 				echo $result . "\r\n";
 			}
 			//// check water level ////
-			if($data_level > $level_set){
+			else if($data_level > $level_set){
 				echo "check 4";
 				$text = "Water level is too low";
 				// Build message to reply back
@@ -107,7 +107,7 @@ if (!is_null($events['events'])) {
 				echo $result . "\r\n";
 			}
 			//// check power ////
-			if($data_power = "off"){
+			else if($data_power == "off"){
 				echo $data_power;
 				$text = "Power OFF!";
 				// Build message to reply back
